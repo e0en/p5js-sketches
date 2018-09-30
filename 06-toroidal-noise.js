301dfc9c251e4f253090f5acf54b08dcb29b0f67
Yoonseop Kang <e0engoon@gmail.com>
2018-09-30 05:23:03 +0000

    Use spherical instead of toroidal coordinates

--- 06-toroidal-noise.js
+++ 06-toroidal-noise.js
@@ -3,6 +3,17 @@ function setup() {
   sizeY = windowHeight;
   createCanvas(sizeX, sizeY);
   noiseSeed(0);
+
+  sinh_map = new Array(1000);
+  cosh_map = new Array(1000);
+
+  for (i = 0; i < 1000; i++) {
+    sinh_map[i] = sinh(TAU / i - PI);
+  }
+
+  for (i = 0; i < 1000; i++) {
+    cosh_map[i] = cosh(TAU / i);
+  }
 }
 
 function draw() {
@@ -47,9 +58,9 @@ function drawOrbit(x, y, phase) {
   xPhase = 0.1 * TAU * x + phase;
   yPhase = 0.1 * TAU * y + phase;
 
-  sphericalCoordinates(xPhase, yPhase, noisePhases);
+  toroidalCoordinate(xPhase, yPhase, 1.0, noisePhases);
 
-  fieldSize = 8;
+  fieldSize = 2;
   localField = noise(fieldSize * noisePhases[0], fieldSize * noisePhases[1], fieldSize * noisePhases[2]);
   localField = sigmoid(10 * (localField - 0.5));
 
@@ -61,8 +72,33 @@ function sigmoid(x) {
   return 1.0 / (1.0 + exp(-x));
 }
 
-function sphericalCoordinates(phase1, phase2, result) {
-  result[0] = sin(phase1) * cos(phase2);
-  result[1] = sin(phase1) * sin(phase2);
-  result[2] = cos(phase2);
+// check https://en.wikipedia.org/wiki/Toroidal_coordinates
+function toroidalCoordinate(phase1, phase2, tau, result) {
+  if (phase1 > TAU) {
+    while (phase1 > TAU) {
+      phase1 -= TAU;
+    }
+  }
+
+  if (phase2 > TAU) {
+    while (phase2 > TAU) {
+      phase2 -= TAU;
+    }
+  }
+
+  tau_idx = int(tau / TAU * 1000) % 1000;
+  denom = cosh_map[tau_idx] - cos(phase1 - PI);
+  common_coeff = sinh_map[tau_idx] / denom;
+  result[0] = common_coeff * cos(phase2);
+  result[1] = common_coeff * sin(phase2);
+  result[2] = sin(phase1 - PI) / denom;
 }
+
+function sinh(x) {
+  return (exp(x) - exp(-x)) / 2.0;
+}
+
+function cosh(x) {
+  return (exp(x) + exp(-x)) / 2.0;
+}
+
